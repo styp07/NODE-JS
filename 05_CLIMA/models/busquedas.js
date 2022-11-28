7a692cfb15037f74277e2a7d3fbeeb2282aaa502
Jairo Styp Rodriguez <83678813+styp07@users.noreply.github.com>
2022-11-28 15:05:58 -0500

    Correción errores

--- 05_CLIMA/models/busquedas.js
+++ 05_CLIMA/models/busquedas.js
@@ -9,11 +9,35 @@ class Buscquedas{
         //TODO: leer DB si existe
     }
 
+    get paramsMapbox(){
+        return {
+            'language':'es',
+            'access_token':'pk.eyJ1Ijoic3R5cDA3IiwiYSI6ImNsYXp5dG1wOTB5bXMzb205MHZkb2hiMmoifQ.joyzFmPgw1DgV0YFUICoDA',
+            'limt': 5
+        }
+    }
+
     async ciudad(lugar = ''){
         //Petición HTTP
-        const resp = await axios.get('https://reqres.in/api/users?page=2');
-        console.log(resp.data);
-        return []; //Retornar los lugares   
+
+        try {
+
+            const instace = axios.create({
+                baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${lugar}.json`,
+                params: this.paramsMapbox
+            })
+
+            const resp = await instace.get(lugar.json,{
+                headers:{
+                    "accept-encoding": null
+                }
+            });
+            console.log(resp.data);
+            return []; //Retornar los lugares               
+        } catch (error) {
+            return[];
+        }
+
     }
 
 }
