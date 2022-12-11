b446c23838de94fc7e9458c9e376d9542c506268
Jairo Styp Rodriguez <83678813+styp07@users.noreply.github.com>
2022-12-10 22:16:16 -0500

    Add Server

--- 06_WEB_SERVER/index.js
+++ 06_WEB_SERVER/index.js
@@ -1,6 +1,12 @@
 import express from 'express';
 import path from 'path';
-import {fileURLToPath} from 'url';
+import { fileURLToPath } from 'url';
+import hbs from 'hbs'
+import * as dotenv from 'dotenv';// see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();  
+
+const port = process.env.port;
+
 const app = express()
 
 // servir contenido estatico
@@ -10,18 +16,37 @@ const __filename = fileURLToPath(import.meta.url);
 // 👇️ "/home/john/Desktop/javascript"
 const __dirname = path.dirname(__filename);
 
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
 app.use(express.static('public'))
 
+app.get('/', function (req, res) {
+  res.render('home', {
+    nombre: 'Jairo Rodriguez',
+    titulo: 'Curso Node'
+  });
+})
+
 app.get('/generic', function (req, res) {
-    res.sendFile(__dirname + '/public/generic.html')
-  })
+  res.render('generic', {
+    nombre: 'Jairo Rodriguez',
+    titulo: 'Curso Node'
+  });
+})
 
 app.get('/elements', function (req, res) {
-    res.sendFile(__dirname + '/public/elements.html')
-  })
+  res.render('elements', {
+    nombre: 'Jairo Rodriguez',
+    titulo: 'Curso Node'
+  });
+})
 
 app.get('*', function (req, res) {
-    res.sendFile( __dirname + '/public/404.html')
-  })
-  
-app.listen(8080) 
+  res.render('404', {
+    nombre: 'Jairo Rodriguez',
+    titulo: 'Curso Node'
+  });
+})
+
+app.listen(port);
